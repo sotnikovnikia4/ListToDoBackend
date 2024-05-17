7a5c13ea93fb5bf213321ca693b262b36bb8a4a2
sotnikovnikia4 <sotnikovnikita4@gmail.com>
2024-05-17 09:34:32 +0300

    2

--- src/test/java/com/sotnikov/ListToDoBackend/controllers/TaskControllerTest.java
+++ src/test/java/com/sotnikov/ListToDoBackend/controllers/TaskControllerTest.java
@@ -1,16 +1,14 @@
 package com.sotnikov.ListToDoBackend.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sotnikov.ListToDoBackend.dto.TaskDTO;
 import com.sotnikov.ListToDoBackend.models.Task;
 import com.sotnikov.ListToDoBackend.models.User;
 import com.sotnikov.ListToDoBackend.security.UserDetailsImpl;
 import com.sotnikov.ListToDoBackend.services.TasksService;
-import com.sotnikov.ListToDoBackend.util.ChangingTaskValidator;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,39 +22,63 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 class TaskControllerTest {
 
     @Mock
     private TasksService tasksService;
-    @Mock
+
     private ModelMapper modelMapper;
-    @Mock
-    private ChangingTaskValidator changingTaskValidator;
     @InjectMocks
     private TaskController taskController;
 
     private User user;
+    private ObjectMapper objectMapper;
 
 
     private MockMvc mockMvc;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
+        try{
+            setUpAuthentication();
+            setUpTaskController();
+            setUpObjectMapper();
+
+            mockMvc = MockMvcBuilders.standaloneSetup(taskController).build();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void setUpTaskController() throws NoSuchFieldException, IllegalAccessException {
+        modelMapper = new ModelMapper();
+
+        modelMapper = new ModelMapper();
+
+        Field modelMapperField = taskController.getClass().getDeclaredField("modelMapper");
+        modelMapperField.setAccessible(true);
+        modelMapperField.set(taskController, modelMapper);
+    }
+
+    void setUpAuthentication(){
         user = new User(UUID.randomUUID(), "login", "password", "name", LocalDateTime.now());
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 new UserDetailsImpl(user), "password", Collections.emptyList()
         );
 
-        mockMvc = MockMvcBuilders.standaloneSetup(taskController).build();
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+    void setUpObjectMapper(){
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
     }
 
     @Test
@@ -65,19 +87,13 @@ class TaskControllerTest {
         Task task2 = new Task("task2", "name2", "sdfsdf", LocalDateTime.now(), null, 10, true, Collections.emptyList(), UUID.randomUUID());
 
         List<Task> tasks = List.of(task1, task2);
-        List<TaskDTO> taskDTOS =
+        List<TaskDTO> taskDTOS = tasks.stream().map(t -> modelMapper.map(t, TaskDTO.class)).toList();
 
         Mockito.when(tasksService.getTasks(user.getId())).thenReturn(tasks);
 
-        AtomicReference<String> contentResult = new AtomicReference<>("");
-
         mockMvc.perform(get("/tasks"))
                 .andExpect(status().isOk())
-                .andExpect(result -> {
-                    contentResult.set(result.getResponse().getContentAsString());
-                });
-
-        Assertions.assertEquals("", contentResult.get());
+                .andExpect(content().string(objectMapper.writeValueAsString(taskDTOS)));
     }
 }
 
