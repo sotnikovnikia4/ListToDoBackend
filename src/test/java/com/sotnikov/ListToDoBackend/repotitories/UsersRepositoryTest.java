573cf331defc13f9c3fd9512deaea6a41967f26c
sotnikovnikia4 <sotnikovnikita4@gmail.com>
2024-05-24 17:15:52 +0300

    тесты по юсер репозиторию

--- src/test/java/com/sotnikov/ListToDoBackend/repotitories/UsersRepositoryTest.java
+++ src/test/java/com/sotnikov/ListToDoBackend/repotitories/UsersRepositoryTest.java
@@ -2,6 +2,7 @@ package com.sotnikov.ListToDoBackend.repotitories;
 
 import com.sotnikov.ListToDoBackend.models.User;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataM
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,20 +21,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class UsersRepositoryTest {
     @Autowired
     private UsersRepository usersRepository;
+    private User user;
 
-    @Test
-    void whenSaveThenReturnSavedEntity(){
-        User user = User.builder()
+    @BeforeEach
+    void setUp(){
+        user = User.builder()
                 .name("John")
                 .login("123")
                 .password("123")
                 .build();
+    }
+
+    @Test
+    void testSaveEntity(){
 
         User savedUser = usersRepository.save(user);
         user.setId(savedUser.getId());
 
         Assertions.assertThat(savedUser).isNotNull();
-        Assertions.assertThat(savedUser).isEqualTo(user);
         Assertions.assertThat(savedUser.getId()).isNotNull();
+    }
+
+    @Test
+    void testFindByLogin(){
+        usersRepository.save(user);
+
+        Optional<User> userByLogin = usersRepository.findByLogin("123");
+
+        Assertions.assertThat(userByLogin.isPresent()).isEqualTo(true);
     }
 }
