9fe47214b3a92d8a49c573459707507cef7bc64f
sotnikovnikia4 <sotnikovnikita4@gmail.com>
2024-05-26 21:20:22 +0300

    пишу тестики

--- src/test/java/com/sotnikov/ListToDoBackend/controllers/UsersControllerTest.java
+++ src/test/java/com/sotnikov/ListToDoBackend/controllers/UsersControllerTest.java
@@ -1,11 +1,18 @@
 package com.sotnikov.ListToDoBackend.controllers;
 
+import ch.qos.logback.core.boolex.EvaluationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sotnikov.ListToDoBackend.config.UserDetailsHolder;
+import com.sotnikov.ListToDoBackend.security.JWTUtil;
 import com.sotnikov.ListToDoBackend.services.UsersService;
+import com.sotnikov.ListToDoBackend.util.EditUserValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,18 +23,29 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(controllers = UsersController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
+@AutoConfigureDataMongo
 class UsersControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private UsersService usersController;
+    private JWTUtil jwtUtil;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @MockBean
+    private EditUserValidator editUserValidator;
+
+    //@MockBean
+    //private ModelMapper modelMapper;
+
+    @MockBean
+    private UsersService usersService;
+
+    @MockBean
+    private UserDetailsHolder userDetailsHolder;
 
     @Test
     void edit() {
+        
     }
 
     @Test
